6bf663fec5a34021e0268fad49ae96382b340b7e
vrecusko <vrecusko20@gmail.com>
2023-08-14 07:36:06 +0200

    use GLFW for app window

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 asagao:
 	-@ mkdir build 2>nul ||:
 # c++23 doesnt exist yet, but g++ allows it for some reason
-	g++ -std=c++23 -O3 -g3 -Wall -Wextra -Werror source\main.cpp -o build\$@.exe
+	g++ -std=c++23 -O3 -g3 -Wall -Wextra -Werror source\main.cpp -o build\$@.exe -Ilibraries\GLFW\inc -Llibraries\GLFW\lib -lopengl32 -lglfw3 -lgdi32
 
 run:
 	.\build\asagao.exe
--- source/main.cpp
+++ source/main.cpp
@@ -3,9 +3,31 @@
 
 #include <iostream>
 
+#include "glfw3.h"
+
+#define VSYNC 1
+
 int main()
 {
-    std::cout << "hi" << std::endl;
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "asagao", nullptr, nullptr);
+
+    if (!window)
+        exit(EXIT_FAILURE);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(VSYNC);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 
     return EXIT_SUCCESS;
 }
