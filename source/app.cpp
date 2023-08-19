abf6e2a5e08ca737f8c92b1a1e114bbe2ed0898e
vrecusko <vrecusko20@gmail.com>
2023-08-19 06:40:29 +0200

    dont waste memory with z=0, everything will be 2D

--- source/app.cpp
+++ source/app.cpp
@@ -25,10 +25,10 @@ void App::run()
 
     const float vertices[]
     {
-        -1.0f, -1.0f, 0.0f,
-         1.0f, -1.0f, 0.0f,
-        -1.0f,  1.0f, 0.0f,
-         1.0f,  1.0f, 0.0f
+        -1.0f, -1.0f,
+         1.0f, -1.0f,
+        -1.0f,  1.0f,
+         1.0f,  1.0f
     };
 
     GLuint indices[]
@@ -51,9 +51,11 @@ void App::run()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
         GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-        nullptr);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 
     while (m_win->is_open())
     {
